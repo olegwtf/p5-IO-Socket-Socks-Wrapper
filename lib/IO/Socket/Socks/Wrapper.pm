bae2923e8ee8c2089c70aa404ce8207352e004ac
Oleg <verdrehung@gmail.com>
2016-10-19 12:37:05 +0600

    check new_from_socket() return value correctly

--- lib/IO/Socket/Socks/Wrapper.pm
+++ lib/IO/Socket/Socks/Wrapper.pm
@@ -246,7 +246,7 @@ sub _connect {
 		$socket->blocking(0);
 	}
 	
-	return unless $ok;
+	return unless $ok || ($io_handler && $IO::Socket::Socks::SOCKS_ERROR == &IO::Socket::Socks::SOCKS_WANT_WRITE);
 	bless $socket, $ref if $ref && !$io_handler; # XXX: should we unbless for GLOB?
 	
 	if ($io_handler) {
